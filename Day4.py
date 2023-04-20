6ad5399bc75c3c63ce6a0ad83dd5ca6718966ed8
Takkiedoe <128038108+Takkiedoe@users.noreply.github.com>
2023-04-20 15:09:42 +0200

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,34 @@
+# part 1
+# import library, get puzzle input
+import hashlib
+input = 'yzbqklnj'
+num = 1
+
+# get key, translate to MD5 hash
+key = input + str(num)
+hash = hashlib.md5(key.encode())
+hex = hash.hexdigest()
+
+# are we there yet?
+while hex[0:5] != '00000':
+    key = input + str(num)
+    hash = hashlib.md5(key.encode())
+    hex = hash.hexdigest()
+    num += 1
+
+print(hex, num-1)
+
+# reset
+num = 1
+key = input + str(num)
+hash = hashlib.md5(key.encode())
+hex = hash.hexdigest()
+
+# are we there yet? part 2
+while hex[0:6] != '000000':
+    key = input + str(num)
+    hash = hashlib.md5(key.encode())
+    hex = hash.hexdigest()
+    num += 1
+
+print(hex, num-1)
